9cbbccd75e3f23e1d9f35e5a25e02caae38b8354
Tornike <72967485+TornikeTt@users.noreply.github.com>
2020-11-13 23:57:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function before() {
+    document.getElementById("myImg").src="images/image-tanya.jpg";
+    document.getElementById("paragraph").innerHTML = '\“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.\ ”';
+    document.getElementById("h4").innerHTML = "Tanya Sinclair";
+    document.getElementById("J").innerHTML = "UX Engineer";
+}
+
+function after() {
+    document.getElementById("myImg").src="images/image-john.jpg";
+    document.getElementById("paragraph").innerHTML = '\“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.\ ”';
+    document.getElementById("h4").innerHTML = "John Tarkpor";
+    document.getElementById("J").innerHTML = "Junior Front-end Developer";
+}
+
+
